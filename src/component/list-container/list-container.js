44e10388024a61c3ec8bcf4a11f428ea7ecee951
vy-shoe <vadimulam6@gatech.edu>
2021-06-13 14:21:57 -0400

    Edit the add item area with multiline text field w materialUI

--- src/component/list-container/list-container.js
+++ src/component/list-container/list-container.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import './list-container.css';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+import AddBoxIcon from '@material-ui/icons/AddBox';
 
 export default function ListContainer(){
     return <div class="list-container">
@@ -15,23 +18,29 @@ export default function ListContainer(){
         <div class="content">
             <ul id="list">
                 <li id="item">
-                <FormControlLabel
-                    control={
-                    <Checkbox
-                        // checked={state.checkedF}
-                        // onChange={handleChange}
-                        name="checkedF"
+                    <FormControlLabel
+                        control={
+                        <Checkbox
+                            // checked={state.checkedF}
+                            // onChange={handleChange}
+                            name="checkedF"
+                        />
+                        }
+                        label="list-item"
                     />
-                    }
-                    label="list-item"
-                />
-                <IconButton aria-label="delete">
-                <DeleteIcon />
-                </IconButton>
+                    <IconButton aria-label="delete">
+                    <DeleteIcon />
+                    </IconButton>
                 </li>
             </ul>
             <div class="add-item">
-                <input type="text" id="input" placeholder="Add your to-do"></input>
+                <AddBoxIcon>AddBox</AddBoxIcon>
+                <TextField
+                    id="standard-textarea"
+                    label="Add a To-Do!"
+                    placeholder="buy milk, workout,etc"
+                    multiline
+                />
             </div>
         </div>
     </div>
